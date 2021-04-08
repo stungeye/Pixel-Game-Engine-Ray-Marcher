3d42d3ade7dfc385cb2e49992e1f5fcb19bf0daa
Kyle Geske <stungeye@gmail.com>
2021-04-08 14:54:32 -0500

    Auto almost all things. Remove C-ism. Normalize all vars to floats. :) Plus some resharper auto cleanup.

--- Pixel Game Engine Ray Marcher/main.cpp
+++ Pixel Game Engine Ray Marcher/main.cpp
@@ -1,169 +1,161 @@
-
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include <vector>
 
 struct vec3 {
-	float x;
-	float y;
-	float z;
+    float x;
+    float y;
+    float z;
 
-	float length() const {
-		return sqrt(x * x + y * y + z * z);
-	}
+    float length() const {
+        return sqrt(x * x + y * y + z * z);
+    }
 
-	void normalize() {
-		float l = length();
-		x = x / l; y = y / l; z = z / l;
-	}
+    void normalize() {
+        const auto l = length();
+        x = x / l;
+        y = y / l;
+        z = z / l;
+    }
 
-	struct vec3 operator*(float fac) const {
-		struct vec3 r;
-		r.x = x * fac; r.y = y * fac; r.z = z * fac;
-		return r;
-	}
+    vec3 operator*(const float& fac) const {
+        return vec3{x * fac, y * fac, z * fac};
+    }
 
-	struct vec3 operator+(struct vec3 other) const {
-		struct vec3 r;
-		r.x = x + other.x; r.y = y + other.y; r.z = z + other.z;
-		return r;
-	}
-	struct vec3 operator-(struct vec3 other) const {
-		struct vec3 r;
-		r.x = x - other.x; r.y = y - other.y; r.z = z - other.z;
-		return r;
-	}
+    vec3 operator+(const vec3& other) const {
+        return vec3{x + other.x, y + other.y, z + other.z};
+    }
+
+    vec3 operator-(const vec3& other) const {
+        return vec3{x - other.x, y - other.y, z - other.z};
+    }
 
 };
 
 // Override base class with your custom functionality
-class BreakOut : public olc::PixelGameEngine
-{
+class RayMarcher : public olc::PixelGameEngine {
 private:
-	float fAccumulatedTime = 0.0f;
-	std::vector<std::vector<double>> framebuffer;
+    float fAccumulatedTime = 0.0F;
+    std::vector<std::vector<float>> frameBuffer;
+    float height{};
+    float width{};
 
 public:
-	BreakOut()
-	{
-		// Name your application
-		sAppName = "Break Out";
-	}
+    RayMarcher() {
+        // Name your application
+        sAppName = "PGE Ray Marcher";
+    }
 
-	float sdf(struct vec3 pos) {
-		struct vec3 center = { 0.0, 0.0, 0.0 };
+    static float sdf(const vec3& pos) {
+        const vec3 center = {0.0, 0.0, 0.0};
+        return (pos - center).length() - 0.5F;
+    }
 
-		return (pos - center).length() - 0.5;
-	}
+    float shade(const vec3& pos, const float t) const {
+        vec3 L = {
+            20.0F * sin(t),
+            20.0F,
+            20.0F * cos(t)
+        };
+        L.normalize();
 
-	float shade(struct vec3 pos, double t) {
-		struct vec3 L = {
-			20.0 * sin(t),
-			20.0,
-			20.0 * cos(t)
-		};
-		L.normalize();
+        const auto dt = 1e-6F;
+        const auto current_val = sdf(pos);
 
-		const float dt = 1e-6;
-		const float current_val = sdf(pos);
+        const vec3 x = {pos.x + dt, pos.y, pos.z};
+        const auto dx = sdf(x) - current_val;
 
-		const struct vec3 x = { pos.x + dt, pos.y, pos.z };
-		const float dx = sdf(x) - current_val;
+        const vec3 y = {pos.x, pos.y + dt, pos.z};
+        const auto dy = sdf(y) - current_val;
 
-		const struct vec3 y = { pos.x, pos.y + dt, pos.z };
-		const float dy = sdf(y) - current_val;
+        const vec3 z = {pos.x, pos.y, pos.z + dt};
+        const auto dz = sdf(z) - current_val;
 
-		const struct vec3 z = { pos.x, pos.y, pos.z + dt };
-		const float dz = sdf(z) - current_val;
+        vec3 N{
+            (dx - pos.x) / dt,
+            (dy - pos.y) / dt,
+            (dz - pos.z) / dt
+        };
 
-		struct vec3 N; // N for normal
-		N.x = (dx - pos.x) / dt;
-		N.y = (dy - pos.y) / dt;
-		N.z = (dz - pos.z) / dt;
+        if (N.length() < 1e-9) {
+            return 0;
+        }
 
-		if (N.length() < 1e-9) {
-			return 0;
-		}
+        N.normalize();
 
-		N.normalize();
+        const auto diffuse{L.x * N.x + L.y * N.y + L.z * N.z};
+        return diffuse * 0.5F + 0.5F; // Remap from -1..1 to 0..1
+    }
 
-		const float diffuse = L.x * N.x + L.y * N.y + L.z * N.z;
-		return pow(diffuse * 0.5 + 0.5, 0.4545) ; // Remap from -1..1 to 0..1
-	}
-
-
-	void RayMarch(double t) {
-		const int height = ScreenHeight();
-		const int width = ScreenWidth();
-
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				struct vec3 pos = { 0.0, 0.0, -3.0 };
-				 // Map x and y to -0.5 to 0.5 and then correct y for aspect ratio.
-				const struct vec3 target = {
-					x / (float)width - 0.5f,
-					(y / (float)height - 0.5f) * (height / (float)width) ,
-						-1.5f
-				};
-
-				struct vec3 ray = (target - pos);
-			    ray.normalize();
-				double pxl = 0.0;
-				const float max = 3.0f; // Started at 9999. We can dial down because there's not much depth to our scene.
-				for (int i = 0; i < 15000; i++) {
-					if (fabs(pos.x) > max
-						|| fabs(pos.y) > max
-						|| fabs(pos.z) > max)
-						break;
-
-					const float dist = sdf(pos);
-					if (dist < 1e-3) { // Started as 1e-6. Shape gets chopping at 1e-2 and 1e-1
-						pxl = shade(pos, t);
-						break;
-					}
-
-					pos = pos + ray * dist;
-				} // end for (i)
-
-				framebuffer[x][y]= pxl;
-			} // end for(x)
-		} // end for(y)
-	} // end raymarch()
-
-
-	bool OnUserCreate() override
-	{
-		// Called once at the start, so create things here
-		framebuffer.resize(ScreenWidth());
-		for(auto i = 0; i < ScreenWidth(); ++i) {
-			framebuffer[i].resize(ScreenHeight());
-		}
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		// Erase previous frame
-		Clear(olc::BLACK);
-		fAccumulatedTime += fElapsedTime;
-		RayMarch(fAccumulatedTime);
-		const int height = ScreenHeight();
-		const int width = ScreenWidth();
+    void RayMarch(const float t) {
         for (auto y = 0; y < height; ++y) {
-			
             for (auto x = 0; x < width; ++x) {
-				const double p = framebuffer[x][y] * 255;
-				Draw(x, y, olc::Pixel(p, 0, 0));
-		   }
-		}
+                vec3 pos = {0.0, 0.0, -3.0};
+                // Map x and y to -0.5 to 0.5 and then correct y for aspect ratio.
+                const struct vec3 target = {
+                    x / width - 0.5F,
+                    (y / height - 0.5F) * (height / width),
+                    -1.5F
+                };
+
+                auto ray = (target - pos);
+                ray.normalize();
+                auto pxl = 0.0F;
+                const auto max = 3.0F; // Started at 9999. We can dial down because there's not much depth to our scene.
+                for (auto i = 0; i < 15000; i++) {
+                    if (std::abs(pos.x) > max
+                        || std::abs(pos.y) > max
+                        || std::abs(pos.z) > max) {
+                        break;
+                    }
+
+                    const auto dist = sdf(pos);
+                    if (dist < 1e-3) {
+                        // Started as 1e-6. Sphere gets chopping at 1e-2 and 1e-1
+                        pxl = shade(pos, t);
+                        break;
+                    }
+
+                    pos = pos + ray * dist;
+                } // end for (i)
+                frameBuffer[x][y] = pxl;
+            } // end for(x)
+        } // end for(y)
+    } 
+
+
+    bool OnUserCreate() override {
+        height = ScreenHeight();
+        width = ScreenWidth();
+
+        frameBuffer.resize(width);
+        for (auto i = 0; i < width; ++i) {
+            frameBuffer[i].resize(height);
+        }
+
         return true;
-	}
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override {
+        // Erase previous frame
+        Clear(olc::BLACK);
+        fAccumulatedTime += fElapsedTime;
+        RayMarch(fAccumulatedTime);
+
+        for (auto y = 0; y < height; ++y) {
+            for (auto x = 0; x < width; ++x) {
+                const auto p = frameBuffer[x][y] * 255;
+                Draw(x, y, olc::Pixel(p, 0, 0));
+            }
+        }
+        return true;
+    }
 };
 
-int main()
-{
-	BreakOut demo;
-	if (demo.Construct(480, 360, 1, 1))
-		demo.Start();
-	return 0;
+int main() {
+    RayMarcher rayMarcher;
+    if (rayMarcher.Construct(480, 360, 1, 1) != 0) {
+        rayMarcher.Start();
+    }
+    return 0;
 }
